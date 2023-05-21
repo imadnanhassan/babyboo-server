09a630b416b5aecb4b6927f8a4f0a4c468ab76a0
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-21 21:27:31 +0600

    add toy single user data

--- index.js
+++ index.js
@@ -33,6 +33,20 @@ async function run() {
          res.send(result);
       });
 
+      app.patch('/addtoy/:id', async (req, res) => {
+         const id = req.params.id;
+         const filter = { _id: new ObjectId(id) };
+         const updatedBooking = req.body;
+         console.log(updatedBooking);
+         const updateDoc = {
+             $set: {
+                 status: updatedBooking.status
+             },
+         };
+         const result = await bookingCollection.updateOne(filter, updateDoc);
+         res.send(result);
+     })
+
 
 
 
