05836f9554f0e5a431eb336bcee7c765705ab033
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-23 03:09:13 +0600

    addtoy, mytoy, viewDetails

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,24 +23,36 @@ const client = new MongoClient(uri, {
 
 async function run() {
    try {
-      await client.connect();
+      //  client.connect();
 
       const addToyCollection = client.db('babybo').collection('babybos');
+
+
+      app.get('/allToy', async (req, res) => {
+         const toy = await addToyCollection.find({}).toArray();
+         res.send(toy)
+      })
+
+      app.get('/viewdetails/:id', async (req, res) => {
+         const id = req.params.id
+         const toy = await addToyCollection.findOne({ _id: new ObjectId(id) })
+         res.send(toy)
+      })
+
+      app.get("/mytoy/:userEmail", async(req, res)=>{
+         const userEmail = req.params.userEmail;
+         const myToy = await addToyCollection.find({sellerEmail: userEmail}).toArray();
+         res.send(myToy)
+       });
+
+
+      app.post('/addToy', async (req, res) => {
+         const toy = req.body
+         const result = await addToyCollection.insertOne(toy);
+         res.send(result)
+      })
+
       
-      app.get('/services/:id', async (req, res) => {
-         const id = req.params.id;
-         const query = { _id: new ObjectId(id) }
-
-         const options = {
-             // Include only the `title` and `imdb` fields in the returned document
-             projection: { title: 1, price: 1, service_id: 1, img: 1 },
-         };
-
-         const result = await serviceCollection.findOne(query, options);
-         res.send(result);
-     })
-
-
 
 
       await client.db("admin").command({ ping: 1 });
